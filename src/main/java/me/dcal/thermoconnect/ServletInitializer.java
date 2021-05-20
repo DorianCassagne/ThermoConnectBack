7dd701038e7ed58accf87e1923b4997468760511
Cassagne Dorian <dmt.cassagne@gmail.com>
2021-05-20 18:43:17 +0200

    bla

--- src/main/java/me/dcal/thermoconnect/ServletInitializer.java
+++ src/main/java/me/dcal/thermoconnect/ServletInitializer.java
@@ -1,13 +1,13 @@
-package me.dcal.thermoconnect;
-
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-public class ServletInitializer extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(ThermoconnectApplication.class);
-	}
-
-}
+//package me.dcal.thermoconnect;
+//
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+//
+//public class ServletInitializer extends SpringBootServletInitializer {
+//
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//		return application.sources(ThermoconnectApplication.class);
+//	}
+//
+//}
--- src/main/java/me/dcal/thermoconnect/ThermoconnectApplication.java
+++ src/main/java/me/dcal/thermoconnect/ThermoconnectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ThermoconnectApplication {
 
 	public static void main(String[] args) {
+    	System.out.println("pipi");
 		SpringApplication.run(ThermoconnectApplication.class, args);
 	}
 
