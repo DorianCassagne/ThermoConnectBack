6179b19fc64d3ad264dbbfbbc14ec8f477d962b6
altristid <aristide.lame2@gmail.com>
2021-05-20 20:34:05 +0200

    Creation model user + jpa user + control user base

---
+++ src/main/java/me/dcal/thermoconnect/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package me.dcal.thermoconnect.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import me.dcal.thermoconnect.model.User;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+@Repository
+public interface  UserRepository extends JpaRepository<User,String>
+{
+	Optional<User> findById(String username);
+	List<User> findAll();
+}
