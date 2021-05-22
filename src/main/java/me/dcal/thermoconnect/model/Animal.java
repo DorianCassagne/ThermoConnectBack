f5914dfb229632d9512c9da30cc73ed19a7e2018
altristid <aristide.lame2@gmail.com>
2021-05-22 17:53:35 +0200

    nouveau ajouts

--- src/main/java/me/dcal/thermoconnect/model/Animal.java
+++ src/main/java/me/dcal/thermoconnect/model/Animal.java
@@ -4,13 +4,14 @@ import java.sql.Date;
 
 public class Animal {
 int idAnimal;
-int idTerrarium;
-int idSpecies;
+Terrarium idTerrarium;
+Species idSpecies;
 String nameAnimal;
 boolean sex;
 Date dateOfBirth;
 String description;
 String text;
 String urlPicture;
+
 
 }
