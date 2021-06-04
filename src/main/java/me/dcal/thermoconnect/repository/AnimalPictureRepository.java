c7e86eca1655f91b9f6775198839b6c392e8fa00
DorianCassagne <dmt.cassagne@gmail.com>
2021-06-04 21:04:00 +0200

    correction suppresion document animal

--- src/main/java/me/dcal/thermoconnect/repository/AnimalPictureRepository.java
+++ src/main/java/me/dcal/thermoconnect/repository/AnimalPictureRepository.java
@@ -1,12 +1,17 @@
 package me.dcal.thermoconnect.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import me.dcal.thermoconnect.id.AnimalPictureId;
 import me.dcal.thermoconnect.model.AnimalPicture;
+import me.dcal.thermoconnect.model.TerrariumData;
 
 @Repository
 public interface AnimalPictureRepository extends JpaRepository<AnimalPicture,AnimalPictureId>{
-
+	@Query(value = "select * from thermoconnect.animal_picture where id_animal = ?1 ",nativeQuery = true)
+	List<AnimalPicture> findPictureByIdAnimal(Integer integer);
 }
